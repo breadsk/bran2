e5e8331601f34591da85208546f5741295f35dfa
Nicolas <nicolas.programador@gmail.com>
2026-07-29 01:12:16 -0400

    acciones de personaje y salto

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,12 +8,19 @@ var gravedad = 400
 #procesador de fisicas
 #es un bucle que se repita cada delta tiempo
 func _physics_process(delta):
+	
+	velocity.y += gravedad*delta
+	#Mientras yo tenga presionada la tecla
 	if Input.is_action_pressed("derecha"):
 		velocity.x = velocidad
-		pass
 	elif Input.is_action_pressed("izquierda"):
 		velocity.x = -velocidad
 	else:
 		velocity.x = 0
+		
+	if is_on_floor():
+		#solo se basta que se presione
+		if Input.is_action_just_pressed("saltar"):
+			velocity.y = -salto
 		
 	move_and_slide()#mover y deslizar el cuerpo
